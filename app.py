338bc6d2e51c450c04a092bda9f62d5541f4b75f
Vlad Solovey <vlad.solovey7@gmail.com>
2025-06-28 10:01:43 +0300

    Migrated to FastAPI

---
+++ app.py
@@ -0,0 +1,128 @@
+import os, redis, requests, json
+from dotenv import load_dotenv
+from datetime import timedelta
+
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import OAuth2PasswordRequestForm
+
+from utils.auth import authenticate_user, create_access_token, decode_token
+
+
+from models.token import Token, TokenData, WelcomeMessage
+
+app = FastAPI()
+
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"], # Allows all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"]  # Allows all headers (Authorization, etc.)
+)
+
+load_dotenv()
+
+@app.post("/token", response_model=Token)
+def login(form_data: OAuth2PasswordRequestForm = Depends()) -> Token: # Parses a form with username, password. Request example: username=franz&password=secret
+    if not form_data.username or not form_data.password:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Missing credentials')
+    
+    if not authenticate_user(form_data.username, form_data.password): # form_data.username = "franz", form_data.password = "secret"
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password")
+    
+    access_token = create_access_token(
+        data = {"sub": form_data.username},
+        expires_delta=timedelta(minutes=15)
+    )
+    return {"access_token": access_token, "token_type": "bearer"} # token_type: bearer means the client should send the token like Authorization: Bearer <token>
+                                                                  # "bearer" means "anyone who has this token is authorized - no password required" 
+
+@app.get("/protected", response_model=WelcomeMessage)
+def protected_route(current_user: TokenData = Depends(decode_token)) -> WelcomeMessage:
+    return {"message": f"Welcome, {current_user.username}"}
+
+redis_client = redis.Redis(host="localhost", port=6379, db=0)
+API_KEY = os.getenv("API_KEY")
+BASE_URL = os.getenv("BASE_URL")
+
+@app.get("/weather/hourly/check-address/{location}")
+def check_address(location: str):
+    redis_key = f"checkAddress:{location}"
+    cached_data = redis_client.get(redis_key)
+
+    if cached_data:
+        return json.loads(cached_data)
+    
+    url = f'{BASE_URL}/{location}'
+    params = {
+        "unitGroup" : "metric",
+        "key" : API_KEY,
+        "include" : "address,resolvedAddress",
+        "elements": "address,resolvedAddress",
+        "content-type" : "json",
+        "locationMode" : "single"
+    }
+
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status() # Raises an error, if occured. Without it, there wouldn't be an error, and except isn't executed
+    except:
+        raise HTTPException(status_code=502, detail="Failed to fetch data from an API") 
+
+    weather_data = response.json()
+    address = weather_data.get('address')
+
+    if not address:
+        raise ValueError('Invalid address')
+
+    redis_client.setex(
+        name=redis_key, 
+        time=timedelta(seconds=3600),
+        value=json.dumps(address)
+    )
+
+    return {"address": address}
+
+@app.get("/weather/hourly/{location}/{number_of_days}")
+def get_weather_hourly(location: str, number_of_days: int):
+    redis_key = f"weatherHourly:{location}:{number_of_days}"
+    cached_data = redis_client.get(redis_key)
+    if cached_data:
+        return {"weather_data": json.loads(cached_data)}
+    
+    url = f"{BASE_URL}/{location}"
+    params = {
+        "unitGroup" : "metric",
+        "key" : API_KEY,
+        "include" : "hours,resolvedAddress",
+        "elements": "address,datetime,temp,feelslike,conditions,preciptype,icon,windspeed,uvindex,sunrise,sunset",
+        "content-type" : "json",
+        "locationMode" : "single"
+    }
+
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status() # Raises an error, if occured. Without it, there wouldn't be an error, and except isn't executed
+    except:
+        raise HTTPException(status_code=502, detail="Failed to fetch data from an API")
+        
+    weather_data_raw = response.json()
+    weather_data_refined = {
+        "address" : weather_data_raw.get("address"),
+        "resolvedAddress" : weather_data_raw.get("resolvedAddress"),
+        "days" : weather_data_raw.get("days", [])[:number_of_days]
+    }
+
+    redis_client.setex(
+        name=redis_key,
+        time=timedelta(hours=1),
+        value=json.dumps(weather_data_refined)
+    )
+
+    return {"weather_data": weather_data_refined}
---
+++ utils/auth.py
@@ -0,0 +1,42 @@
+import os
+from dotenv import load_dotenv
+
+from datetime import datetime, timedelta, timezone
+
+from jose import JWTError, jwt
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+
+from models.token import TokenData
+from models.user import User
+
+load_dotenv()
+
+JWT_SECRET = os.getenv('JWT_SECRET')
+ALGORITHM = os.getenv('ALGORITHM')
+ACCESS_TOKEN_EXPIRES_MINUTES = int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES'))
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token") # OAuth2PasswordBearer extracts token from Authorization: Bearer
+                                                                # tokeUnrl='token' is the login endpoint that provides the token('/token')
+                                                                # oath2_scheme tell fastAPI how to extract token from Authorization header
+
+fake_user: User = {"username": "franz", "password": "secret"}
+
+def authenticate_user(username: str, password: str) -> bool:
+    return username == fake_user["username"] and password == fake_user["password"]
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str: #data can be {'sub':'franz'}
+    to_encode = data.copy() # to prevent modifying original dict, which could cause bugs if it's reused somewhere
+    expires_at = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=15))
+    to_encode.update({"exp": expires_at})
+    return jwt.encode(to_encode, JWT_SECRET, algorithm=ALGORITHM)
+
+def decode_token(token: str = Depends(oauth2_scheme)) -> TokenData:
+    try:
+        payload = jwt.decode(token, JWT_SECRET, algorithms=ALGORITHM)
+        username: str = payload.get("sub") # subject of the token (username)
+        if username is None:
+            raise HTTPException(status_code=400, detail="Invalid token")
+        return TokenData(username=username)
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid or expired token")
